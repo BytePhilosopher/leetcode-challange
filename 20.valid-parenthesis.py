c66e77e8f4cef0b1997ebc099a8e6d8e5acfd673
BytePhilosopher <aberayostina@gmail.com>
2025-08-03 17:13:56 +0300

    number 20

--- 20.valid-parenthesis.py
+++ 20.valid-parenthesis.py
@@ -43,3 +43,21 @@
 
 # 1 <= s.length <= 104
 # s consists of parentheses only '()[]{}'.
+
+
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        bracket_map = {')': '(', ']': '[', '}': '{'}
+
+        for char in s:
+            if char in bracket_map.values():  # Opening brackets
+                stack.append(char)
+            elif char in bracket_map:  # Closing brackets
+                if not stack or stack[-1] != bracket_map[char]:
+                    return False
+                stack.pop()
+            else:
+                return False  # Invalid character
+
+        return len(stack) == 0
