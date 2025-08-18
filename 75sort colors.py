691a7a7df44f7b4fe73aec29cc79c3e2bf7e0c5a
BytePhilosopher <aberayostina@gmail.com>
2025-08-18 17:24:33 +0300

    init commit

---
+++ 75sort colors.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # Step 1: Count occurrences of 0,1,2
+        count = [0, 0, 0]
+        for n in nums:
+            count[n] += 1
+
+        # Step 2: Overwrite nums in sorted order
+        i = 0
+        for color in range(3):           # only 0,1,2
+            for j in range(count[color]):
+                nums[i] = color
+                i += 1
+
+# Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+# We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+# You must solve this problem without using the library's sort function.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,0,2,1,1,0]
+# Output: [0,0,1,1,2,2]
+# Example 2:
+
+# Input: nums = [2,0,1]
+# Output: [0,1,2]
+ 
