8d5def84451d3c3e5762d9b3be31928c425170cf
BytePhilosopher <aberayostina@gmail.com>
2025-08-24 00:05:24 +0300

    init commit

---
+++ 199 binary tree ryt side view.py
@@ -0,0 +1,73 @@
+# Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+ 
+
+# Example 1:
+
+# Input: root = [1,2,3,null,5,null,4]
+
+# Output: [1,3,4]
+
+# Explanation:
+
+
+
+# Example 2:
+
+# Input: root = [1,2,3,4,null,null,null,5]
+
+# Output: [1,3,4,5]
+
+# Explanation:
+
+
+
+# Example 3:
+
+# Input: root = [1,null,3]
+
+# Output: [1,3]
+
+# Example 4:
+
+# Input: root = []
+
+# Output: []
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        res = []
+        queue = deque()
+
+        if root:
+            queue.append(root)
+        
+        while queue:
+            level_size = len(queue)
+            for i in range(level_size):   # Process one level
+                curr = queue.popleft()
+                
+                # if it's the last node of this level, add it to result
+                if i == level_size - 1:
+                    res.append(curr.val)
+
+                # Normal BFS traversal order (left first, then right)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+        
+        return res
