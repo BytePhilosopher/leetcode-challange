20c5578dc5feb395d63388d8adcd85866634e745
BytePhilosopher <aberayostina@gmail.com>
2025-08-23 20:11:50 +0300

    init commit

--- 105 construct BT from preorder and inorder.py
+++ 105 construct BT from preorder and inorder.py
@@ -23,11 +23,11 @@
 # preorder is guaranteed to be the preorder traversal of the tree.
 # inorder is guaranteed to be the inorder traversal of the tree.
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution(object):
     def buildTree(self, preorder, inorder):
