f1df1aa2c9ace214249a9b5f0be06d118b02a197
BytePhilosopher <aberayostina@gmail.com>
2025-08-23 18:48:40 +0300

    init commit

---
+++ 230.kth smallest element in BST.py
@@ -0,0 +1,52 @@
+# Definition for a binary tree node.
+# Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+ 
+
+# Example 1:
+
+
+# Input: root = [3,1,4,null,2], k = 1
+# Output: 1
+# Example 2:
+
+
+# Input: root = [5,3,6,2,4,null,null,1], k = 3
+# Output: 3
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is n.
+# 1 <= k <= n <= 104
+# 0 <= Node.val <= 104
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: Optional[TreeNode]
+        :type k: int
+        :rtype: int
+        """
+        self.count = 0
+        self.result = None
+
+        def inorder(node):
+            if not node or self.result is not None:
+                return
+            inorder(node.left)
+            
+            self.count += 1
+            if self.count == k:   # found kth smallest
+                self.result = node.val
+                return
+            
+            inorder(node.right)
+
+        inorder(root)
+        return self.result
