189c9a83499dbb321efe2aecb18c50b89a5e9f09
BytePhilosopher <aberayostina@gmail.com>
2025-09-27 11:26:16 +0300

    init commit

---
+++ 203. movw Zeros.py
@@ -0,0 +1,35 @@
+
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Note that you must do this in-place without making a copy of the array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [0,1,0,3,12]
+# Output: [1,3,12,0,0]
+# Example 2:
+
+# Input: nums = [0]
+# Output: [0]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 104
+# -231 <= nums[i] <= 231 - 1
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        l=0
+        for i in range(len(nums)):
+            if nums[l]==0:
+                del nums[l]
+                nums.append(0)
+            else:
+                l+=1
+        
