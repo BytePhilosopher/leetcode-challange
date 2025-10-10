f1739267902938b674347ff190ad0bd6a2d49d1c
BytePhilosopher <aberayostina@gmail.com>
2025-10-10 20:43:10 +0300

    init commit

---
+++ 918. Maximum Sum Circular Subarray.py
@@ -0,0 +1,55 @@
+# Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
+
+# A circular array means the end of the array connects to the beginning of the array. Formally, the next element of nums[i] is nums[(i + 1) % n] and the previous element of nums[i] is nums[(i - 1 + n) % n].
+
+# A subarray may only include each element of the fixed buffer nums at most once. Formally, for a subarray nums[i], nums[i + 1], ..., nums[j], there does not exist i <= k1, k2 <= j with k1 % n == k2 % n.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,-2,3,-2]
+# Output: 3
+# Explanation: Subarray [3] has maximum sum 3.
+# Example 2:
+
+# Input: nums = [5,-3,5]
+# Output: 10
+# Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10.
+# Example 3:
+
+# Input: nums = [-3,-2,-3]
+# Output: -2
+# Explanation: Subarray [-2] has maximum sum -2.
+ 
+
+# Constraints:
+
+# n == nums.length
+# 1 <= n <= 3 * 104
+# -3 * 104 <= nums[i] <= 3 * 104
+class Solution(object):
+    def maxSubarraySumCircular(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxsum=nums[0]
+        cursum=0
+        minsum=float('inf')
+        curmin=0
+        total_sum = sum(nums)
+        for n in nums:
+            cursum=max(cursum,0)
+            cursum+=n
+            maxsum=max(maxsum,cursum)
+        for n in nums:
+            curmin=min(curmin,0)
+            curmin+=n
+            minsum=min(minsum,curmin)
+        result=max(maxsum,total_sum-minsum)
+        if maxsum < 0:
+            return maxsum
+        return result
+
+        
